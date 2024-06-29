0fa0c1cf482196cf26e0a600c1657335a4b7ea24
naoyuki-miyamori <nayouki.miyamori@conct.co.jp>
2024-06-29 19:49:29 +0900

    s3への保存でのイベントを追加

--- app/index.php
+++ app/index.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
 
+use Aws\Result;
+use Aws\S3\S3Client;
 use Bref\Context\Context;
 use Bref\Event\InvalidLambdaEvent;
 use Bref\Event\S3\S3Event;
@@ -11,6 +13,7 @@ use Bref\Event\S3\S3Handler;
 use Bref\Logger\StderrLogger;
 use Psr\Log\LoggerInterface;
 use Psr\Log\LogLevel;
+use ZBateson\MailMimeParser\MailMimeParser;
 
 /**
  * S3のEventハンドラー
@@ -18,16 +21,40 @@ use Psr\Log\LogLevel;
 class Handler extends S3Handler
 {
     /**
+     * @var string
+     */
+    private const BUCKET_NAME = 'bref-php-sample';
+
+    /**
+     * @var string
+     */
+    private const BUCKET_KEY_PREFIX = 'convert/';
+
+    /**
      * @var LoggerInterface
      */
     private readonly LoggerInterface $logger;
 
     /**
-     * @param LoggerInterface $logger
+     * @var MailMimeParser
      */
-    public function __construct(LoggerInterface $logger)
+    private readonly MailMimeParser $mailParser;
+
+    /**
+     * @var S3Client
+     */
+    private readonly S3Client $s3Client;
+
+    /**
+     * @param LoggerInterface $logger
+     * @param MailMimeParser $mailParser
+     * @param S3Client $s3Client
+     */
+    public function __construct(LoggerInterface $logger, MailMimeParser $mailParser, S3Client $s3Client)
     {
-        $this->logger = $logger;
+        $this->logger     = $logger;
+        $this->mailParser = $mailParser;
+        $this->s3Client   = $s3Client;
     }
 
     /**
@@ -38,12 +65,60 @@ class Handler extends S3Handler
      */
     public function handleS3(S3Event $event, Context $context): void
     {
-        $bucketName = $event->getRecords()[0]->getBucket()->getName();
-        $fileName = $event->getRecords()[0]->getObject()->getKey();
+        $this->parseMail($event);
 
-        $this->logger->info($bucketName);
-        $this->logger->warning($fileName);
+//        $result = $this->getFileObject($event);
+//
+//        $this->s3Client->putObject([
+//            'Bucket' => self::BUCKET_NAME,
+//            'Key'    => self::BUCKET_KEY_PREFIX . basename($event->getRecords()[0]->getObject()->getKey()),
+//            'Body'   => $result->get('Body'),
+//        ]);
+    }
+
+    /**
+     * @param S3Event $event
+     *
+     * @return Result
+     *
+     * @throws InvalidLambdaEvent
+     */
+    private function getFileObject(S3Event $event): Result
+    {
+        $record = $event->getRecords()[0];
+
+        return $this->s3Client->getObject([
+            'Bucket' => self::BUCKET_NAME,
+            'Key'    => $record->getObject()->getKey(),
+        ]);
+    }
+
+    /**
+     * @param S3Event $event
+     *
+     * @return void
+     *
+     * @throws InvalidLambdaEvent
+     */
+    private function parseMail(S3Event $event): void
+    {
+        $mail    = $event->getRecords()[0]->getObject();
+        $message = $this->mailParser->parse($mail, false);
+
+        for ($i = 0; $i < $message->getAttachmentCount(); $i++) {
+            $part = $message->getAttachmentPart($i);
+
+            if ($part === null) {
+                continue;
+            }
+
+            $this->logger->info($part->getContent());
+        }
     }
 }
 
-return new Handler(new StderrLogger(LogLevel::INFO));
+return new Handler(
+    new StderrLogger(LogLevel::INFO),
+    new MailMimeParser(),
+    new S3Client(['region' => 'ap-northeast-1'])
+);
